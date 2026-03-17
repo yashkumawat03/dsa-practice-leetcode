f53ff30f00adac70a84dfd9aa9fde5b31c3d2fc9
yashkumawat03 <152025776+yashkumawat03@users.noreply.github.com>
2026-03-17 23:58:00 +0530

    Time: 3 ms (86.33%), Space: 138.9 MB (24.02%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,54 +1,21 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        //int n = nums.size();
-        // for(int i=0;i<n;i++){
-        //     //int ele = 0;
-        //     if(i%2==0){
-        //         if(nums[i]<0){
-        //             int ele = nums[i];
-        //             //int j=i+1;
-        //             for(int j=i+1;j<n;j++){
-        //                 if(nums[j]>0){
-        //                     nums[i]=nums[j];
-        //                     nums[j]=ele;
-        //                     break;
-        //                 }
-        //             }
-                        
-        //         }
-        //     }
-             
-        //     else if(i%2 != 0){
-        //         if(nums[i]>0){
-        //            int ele = nums[i];
-        //             for(int j=i+1;j<n;j++){
-        //                 if(nums[j]<0){
-        //                     nums[i]=nums[j];
-        //                     nums[j]=ele;
-        //                     break;
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        // return nums;
-        int n = nums.size();
-        vector<int> ans(n);
-
-        int posIndex = 0; // even indices
-        int negIndex = 1; // odd indices
-
-        for(int x : nums){
-            if(x > 0){
-                ans[posIndex] = x;
-                posIndex += 2;
-            } else {
-                ans[negIndex] = x;
-                negIndex += 2;
+        int n =nums.size();
+        vector<int> pos;
+        vector<int> neg;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                pos.push_back(nums[i]);
+            }
+            else{
+                neg.push_back(nums[i]);
             }
         }
-
-        return ans;
+        for(int i=0;i<n/2;i++){
+            nums[2*i]=pos[i];
+            nums[2*i+1]=neg[i];
+        }
+        return nums;
     }
 };
