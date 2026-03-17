c19dc3d57e336ecebb57fd894255f731afa39ced
yashkumawat03 <152025776+yashkumawat03@users.noreply.github.com>
2026-03-17 11:18:37 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,7 +6,9 @@ public:
         int sum = 0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            maxi=max(sum,maxi);
+            if(sum>maxi){
+                maxi = sum;
+            }
             if(sum<0) sum = 0;
         }
         return maxi;
