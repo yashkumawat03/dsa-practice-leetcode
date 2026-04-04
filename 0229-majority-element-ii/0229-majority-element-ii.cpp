e0fb40b2cb68aaf7f94ba51615c76ab4cac7f930
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-04-04 22:48:27 +0530

    Time: 13 ms (11.67%), Space: 26.3 MB (36.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1 = 0, cnt2 = 0;
-        int ele1 = INT_MIN, ele2 = INT_MIN;
-
-        for(int num : nums){
-            if(num == ele1) cnt1++;
-            else if(num == ele2) cnt2++;
-            else if(cnt1 == 0){
-                ele1 = num;
-                cnt1 = 1;
-            }
-            else if(cnt2 == 0){
-                ele2 = num;
-                cnt2 = 1;
-            }
-            else{
-                cnt1--;
-                cnt2--;
-            }
-        }
-
-        cnt1 = 0, cnt2 = 0;
-        for(int num : nums){
-            if(num == ele1) cnt1++;
-            else if(num == ele2) cnt2++;
-        }
-
-        vector<int> ans;
         int n = nums.size();
-
-        if(cnt1 > n/3) ans.push_back(ele1);
-        if(cnt2 > n/3) ans.push_back(ele2);
-
+        vector<int> ans;
+        // for(int i=0;i<n;i++){
+        //     if(ans.size()==0 || ans[0]!=nums[i]){
+        //         int element = nums[i];
+        //         int cnt = 1;
+        //         for(int j=i+1;j<n;j++){
+        //             if(nums[i]==nums[j]){
+        //             cnt++;
+        //             }
+        //         }
+        //         if(cnt>n/3)  ans.push_back(nums[i]);
+        //     }
+        //     if(ans.size()==2) break;
+        // }
+        unordered_map<int,int> mpp;
+        int min = n/3+1;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]==min) ans.push_back(nums[i]);
+            if(ans.size()==2) break;
+            
+        }
+        // for(auto it : mpp){
+        //     if(it.second>n/3) ans.push_back(it.first);
+        // }
         return ans;
     }
 };
