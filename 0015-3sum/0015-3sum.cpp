3eb6d0c13385c869a7385e3fd3941dcb1db8bd69
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-04-06 19:47:03 +0530

    Time: 57 ms (22.68%), Space: 31 MB (20.68%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,41 +1,60 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
         int n = nums.size();
+        //set<vector<int>> st;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         for(int k=j+1;k<n;k++){
+        //             if(nums[i]+nums[j]+nums[k]==0){
+        //                 vector<int> temp = {nums[i],nums[j],nums[k]};
+        //                 sort(temp.begin(),temp.end());
+        //                 st.insert(temp);
+        //             }
+        //         }
+        //     }
+        // }
+        // vector<vector<int>> ans(st.begin(),st.end());
+        // return ans;
 
-        sort(nums.begin(), nums.end());
+        // Better approach-Use hashing
+        // for(int i=0;i<n;i++){
+        //     set<int> hashset;
+        //     for(int j=i+1;j<n;j++){
+        //         int rem = -(nums[i]+nums[j]);
+        //         if(hashset.find(rem)!=hashset.end()){
+        //             vector<int> temp = {nums[i],nums[j],rem};
+        //             sort(temp.begin(),temp.end());
+        //             st.insert(temp);
+        //         }
+        //         hashset.insert(nums[j]);
+        //     }
+        // }
+        // vector<vector<int>> ans(st.begin(),st.end());
+        // return ans;
 
-        for(int i = 0; i < n; i++){
-            // Skip duplicates for i
-            if(i > 0 && nums[i] == nums[i-1]) continue;
 
-            int j = i + 1;
-            int k = n - 1;
-
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if(sum < 0){
-                    j++;
-                }
-                else if(sum > 0){
-                    k--;
-                }
+        //Optimal Approach - Using two pointer approach
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum>0) k--;
+                else if(sum<0) j++;
                 else{
-                    ans.push_back({nums[i], nums[j], nums[k]});
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
                     j++;
                     k--;
-
-                    // Skip duplicates for j
-                    while(j < k && nums[j] == nums[j-1]) j++;
-
-                    // Skip duplicates for k
-                    while(j < k && nums[k] == nums[k+1]) k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
-
         return ans;
     }
 };
