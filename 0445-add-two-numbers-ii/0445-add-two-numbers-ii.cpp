236312659ea21ab23156655bd23516622a72a1cc
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-05-02 23:41:30 +0530

    Time: 2 ms (54.3%), Space: 79.2 MB (42.39%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+
+        // Push all values into stacks
+        while (l1) {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while (l2) {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+
+        int carry = 0;
+        ListNode* head = nullptr;
+
+        // Process stacks
+        while (!s1.empty() || !s2.empty() || carry) {
+            int sum = carry;
+
+            if (!s1.empty()) {
+                sum += s1.top();
+                s1.pop();
+            }
+            if (!s2.empty()) {
+                sum += s2.top();
+                s2.pop();
+            }
+
+            // Create new node
+            ListNode* node = new ListNode(sum % 10);
+            node->next = head;
+            head = node;
+
+            carry = sum / 10;
+        }
+
+        return head;
+    }
+};
