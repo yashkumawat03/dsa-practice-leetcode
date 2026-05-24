b6b83f0cc25955ab4dd044b273dcf9e2f8e96729
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-05-24 20:17:20 +0530

    Time: 11 ms (34.67%), Space: 23 MB (11.63%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-       int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
-
-        while(low < high){
-            int mid = low + (high - low) / 2;
-
-            long long hours = 0;
-
-            for(int pile : piles){
-                hours += (pile + mid - 1) / mid;
-            }
-
-            if(hours <= h){
-                high = mid;  
-            }
-            else{
-                low = mid + 1;
+    int max(vector<int>& piles){
+        int maxi = INT_MIN;
+        for(int i = 0;i<piles.size();i++){
+            if(piles[i] > maxi){
+                maxi = piles[i];
             }
         }
-
-        return low; 
+        return maxi;
+    }
+    long long func(vector<int>& piles, int mid){
+        long long time = 0;
+        for(int i=0;i<piles.size();i++){
+            time += ceil(double(piles[i])/double(mid));
+        }
+        return time;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int low = 1;
+        int high = max(piles);
+        //int ans = INT_MAX;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            long long required_time = func(piles,mid);
+            if(required_time <= h){
+                //ans = min(mid,ans);
+                high = mid-1;
+            }
+            else
+                low = mid+1;
+        }
+        return low;
     }
 };
