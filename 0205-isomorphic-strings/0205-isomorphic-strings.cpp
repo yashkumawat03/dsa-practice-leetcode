ead94a9c9c9968689bfa5fdf05f94382e786fc8b
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-05-31 11:39:27 +0530

    Time: 0 ms (100%), Space: 9.2 MB (81.45%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> m1;
-        unordered_map<char, char> m2;
+        // unordered_map<char, char> m1;
+        // unordered_map<char, char> m2;
 
-        for(int i = 0; i < s.length(); i++){
-            char c1 = s[i];
-            char c2 = t[i];
+        // for(int i = 0; i < s.length(); i++){
+        //     char c1 = s[i];
+        //     char c2 = t[i];
 
-            // check s -> t
-            if(m1.find(c1) != m1.end()){
-                if(m1[c1] != c2) return false;
-            } 
-            else {
-                m1[c1] = c2;
-            }
+        //     // check s -> t
+        //     if(m1.find(c1) != m1.end()){
+        //         if(m1[c1] != c2) return false;
+        //     } 
+        //     else {
+        //         m1[c1] = c2;
+        //     }
 
-            // check t -> s
-            if(m2.find(c2) != m2.end()){
-                if(m2[c2] != c1) return false;
-            } else {
-                m2[c2] = c1;
+        //     // check t -> s
+        //     if(m2.find(c2) != m2.end()){
+        //         if(m2[c2] != c1) return false;
+        //     } else {
+        //         m2[c2] = c1;
+        //     }
+        // }
+
+        // return true;
+
+        vector<int> map(256,0);
+        vector<bool> visited(256,false);
+        for(int i=0;i<s.size();i++){
+            if(map[s[i]]==0 && visited[t[i]] == false){
+                map[s[i]] = t[i];
+                visited[t[i]] = true;
             }
         }
-
+        for(int i=0;i<s.size();i++){
+            if(map[s[i]] != t[i]) return false;
+        }
         return true;
     }
 };
