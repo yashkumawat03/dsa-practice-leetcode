ee8b571b2e710baef151565962422fa0e473bf58
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-06-01 09:24:59 +0530

    Time: 0 ms (100%), Space: 12.7 MB (38.98%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,13 +9,15 @@ public:
         int high = n-2;
         while(low <= high){
             int mid = (low + high)/2;
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
-            else if(nums[mid] < nums[mid-1]){
-                high = mid-1;
-            }
-            else{
+            if(nums[mid] >nums[mid-1] && nums[mid] > nums[mid+1])
+                return mid;
+            else if(nums[mid] < nums[mid+1]){
                 low = mid+1;
             }
+            else{
+                high = mid-1;
+            }
+            
         }
         return -1;
     }
