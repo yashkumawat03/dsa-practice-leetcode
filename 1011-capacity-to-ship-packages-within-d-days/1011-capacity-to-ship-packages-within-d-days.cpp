e78bc6cf23be82daccaa5ed585ca3d8c4f69a740
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-06-03 00:35:04 +0530

    Time: 7 ms (91.87%), Space: 35 MB (52.47%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    bool ispossible(vector<int> &weights, int mid, int days){
-        int day = 1;
-        int shipload = 0;
-        for(int i=0; i<weights.size();){
-            if(shipload + weights[i] <= mid){
-                shipload += weights[i];
-                i++;
-            }
-            else{
-                day++;
-                shipload = 0;
-            }
-            if(day > days) return false;
-        }
-        return true;
-    }
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
-        int ans;
-        int total = 0;
-        for(int i=0;i<n;i++){
-            total += weights[i];
-        }
-        int s = 1, e = total;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(ispossible(weights,mid,days)){
-                ans = mid;
-                e = mid-1;
+        int low = *max_element(weights.begin(),weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        if(days == 1) return high;
+        int sum = 0;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            int d = 1;
+            int sum = 0;
+            for(int i : weights){
+                if(sum + i > mid){
+                    d++;
+                    sum = i;
+                }
+                else{
+                    sum += i;
+                }
+            }
+            if(d <= days){
+                high = mid - 1;
             }
             else{
-                s = mid + 1;
+                low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
 };
