91abb6b3d4da5385f3ef93c82f6b86031eeaccea
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-06-05 16:49:17 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
+        int n = s.size();
         stack<char> st;
-
-        for(int i = 0; i < s.length(); i++) {
-
-            // Opening brackets
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+        for(int i = 0; i < n; i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
                 st.push(s[i]);
             }
-
-            // Closing brackets
-            else {
-
-                // No matching opening bracket
-                if(st.empty()) {
-                    return false;
+            else{
+                if(st.empty()) return false;
+                char ch = st.top();
+                if((s[i] == ']' && ch == '[') ||
+                    (s[i] == ')' && ch == '(') ||
+                    (s[i] == '}' && ch == '{')){
+                        st.pop();
                 }
-
-                char top = st.top();
-
-                // Matching pairs
-                if((s[i] == ')' && top == '(') ||
-                   (s[i] == '}' && top == '{') ||
-                   (s[i] == ']' && top == '[')) {
-
-                    st.pop();
-                }
-                else {
-                    return false;
-                }
+                else return false;
             }
         }
-
-        // Stack should be empty
         return st.empty();
     }
 };
