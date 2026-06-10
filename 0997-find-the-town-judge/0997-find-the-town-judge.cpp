c6ff86a653f77e330a850cc5d229bbfe64b3e5c6
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-06-10 09:52:03 +0530

    Time: 19 ms (36.17%), Space: 73.2 MB (22.8%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> in(n+1, 0);
+        vector<int> out(n+1, 0);
+        for(auto p : trust){
+            out[p[0]]++;
+            in[p[1]]++;
+        }
+        for(int i = 1; i <= n; i++){
+            if(out[i] == 0 && in[i] == n-1)
+                return i;
+        }
+        return -1;
+    }
+};
