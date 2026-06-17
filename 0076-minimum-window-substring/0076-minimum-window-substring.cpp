98bd02427afa3959e1c9cf1208d30258a10d4083
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-06-17 23:14:34 +0530

    Time: 0 ms (100%), Space: 11.5 MB (64.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,31 +3,36 @@ public:
     string minWindow(string s, string t) {
         int n = s.length();
         int m = t.length();
-        if(m>n) return "";
-        int left = 0, right = 0;
+
         vector<int> freq(256);
         for(char ch : t){
             freq[ch]++;
         }
-        int count = 0,start = 0,len = INT_MAX;
-        while(right<n){
-            if(freq[s[right]]>0){
-                count++;
+        int left = 0;
+        int right = 0;
+        int cnt = 0;
+        int len = INT_MAX;
+        int start = -1;
+        
+        while(right < n){
+            if(freq[s[right]] > 0){
+                cnt++;
             }
             freq[s[right]]--;
-            while(count == m){
-                if(right-left+1 < len){
-                    len = right-left+1;
+
+            while(cnt == m){
+                if(right - left + 1 < len){
                     start = left;
+                    len = right - left + 1;
                 }
                 freq[s[left]]++;
-                if(freq[s[left]]>0)
-                    count--;
+                if(freq[s[left]] > 0)
+                    cnt--;
                 left++;
             }
             right++;
         }
         if(len == INT_MAX) return "";
-        return s.substr(start,len);
+        return s.substr(start, len);
     }
 };
