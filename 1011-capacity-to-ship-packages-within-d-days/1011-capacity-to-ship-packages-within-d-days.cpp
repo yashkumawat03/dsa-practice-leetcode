cc5a981b08a68f100319d85b13852c7bf19a66e2
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-08-18 00:50:06 +0530

    Time: 11 ms (64.79%), Space: 35 MB (54.65%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        int n = weights.size();
-        int low = *max_element(weights.begin(),weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        if(days == 1) return high;
+    int func(vector<int>& weights, int mid){
         int sum = 0;
+        int days = 1;
+        for(int i = 0; i < weights.size(); i++){
+            if(sum + weights[i] > mid){
+                days++;
+                sum = weights[i];
+            }
+            else sum += weights[i];
+        }
+        return days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(),0);
         while(low <= high){
             int mid = low + (high - low) / 2;
-            int d = 1;
-            int sum = 0;
-            for(int i : weights){
-                if(sum + i > mid){
-                    d++;
-                    sum = i;
-                }
-                else{
-                    sum += i;
-                }
-            }
-            if(d <= days){
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
+            int d = func(weights, mid);
+            if(d <= days) high = mid - 1;
+            else low = mid + 1;
         }
         return low;
     }
